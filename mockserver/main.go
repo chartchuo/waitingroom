823fada973327a0a6a9b8d388e0fdf1dd945d164
chartchuo <chartchuo@gmail.com>
2018-09-16 12:36:30 +0700

    auto update config in kube

--- mockserver/main.go
+++ mockserver/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	confManager := NewMutexConfigManager(loadConfig("config/config.yml"))
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -31,11 +36,31 @@ func main() {
 			"message": "destination",
 		})
 	})
+	r.GET("/config", func(c *gin.Context) {
+		conf := confManager.Get()
+		c.JSON(200, gin.H{
+			"message": conf.Message,
+		})
+	})
 	r.GET("/redirect", func(c *gin.Context) {
 		c.Redirect(302, "/destination")
 	})
 	r.GET("/error", func(c *gin.Context) {
 		c.Err()
 	})
+
+	watcher, err := WatchFile("config/config.yml", time.Second*5, func() {
+		log.Printf("Configfile Updated\n")
+		conf := loadConfig("config/config.yml")
+		confManager.Set(conf)
+	})
+	check(err)
+
+	defer func() {
+		watcher.Close()
+		confManager.Close()
+	}()
+
 	r.Run(":8888")
+
 }
