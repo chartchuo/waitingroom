1093d3b5486b385c1f27ec205e09a27d6e0df5b0
chartchuo <chartchuo@gmail.com>
2018-09-16 20:47:45 +0700

    add grpc

--- controller/main.go
+++ controller/main.go
@@ -1,13 +1,42 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"apichart.me/waitingroom/controller/ctl"
+)
+
+type server struct {
+}
+
+func (s *server) CtlStream(stream ctl.Ctl_CtlStreamServer) error {
+	log.Println("Started stream")
+	for {
+		in, err := stream.Recv()
+		log.Println("Received value")
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		log.Printf("sum: %v, count:%v\n", in.Sum, in.Count)
+	}
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	grpcServer := grpc.NewServer()
+	ctl.RegisterCtlServer(grpcServer, &server{})
+
+	l, err := net.Listen("tcp", ":6000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Println("Listening on tcp://localhost:6000")
+	grpcServer.Serve(l)
 }
